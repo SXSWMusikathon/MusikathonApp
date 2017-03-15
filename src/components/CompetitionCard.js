5275da81784ee0dda103605a1079024feb145158
HeyImRige <Robert.aron.tx@verizon.net>
2017-03-14 23:00:46 -0500

    Competition listings and minor CSS fixes

--- src/components/CompetitionCard.js
+++ src/components/CompetitionCard.js
@@ -3,25 +3,29 @@ import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'mat
 import FlatButton from 'material-ui/FlatButton';
 import testImage from '../images/SongArtTest.jpg';
 
-const CardExampleWithAvatar = () => (
-  <Card className="competition-card">
+const CompetitionCard = (props) =>{
+  const {
+    songTitle,
+    artistName,
+    contestDescription
+    } = props;
+  return <Card className="competition-card">
     <CardMedia className="competition-card-image">
       <img src={testImage}/>
     </CardMedia>
-    <CardTitle className="competition-card-title" title="$Song Title" subtitle="$Artist Name" />
+    <CardTitle className="competition-card-title" title={songTitle} subtitle={artistName} />
     <CardText className="competition-card-text">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+      {contestDescription}
     </CardText>
     <CardActions className="competition-card-actions">
+    {/*TODO make the action button a play button*/}
       <FlatButton label="Action2" />
     </CardActions>
   </Card>
-);
+};
 
-export default CardExampleWithAvatar;
+
+export default CompetitionCard;
 
 
 
--- src/containers/CompetitionListPage.js
+++ src/containers/CompetitionListPage.js
@@ -1,13 +1,119 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
+  from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import Toggle from 'material-ui/Toggle';
+import './CompetitionListPage.css';
 
-export default class CompetitionListPage extends Component {
-  constructor(props){
-	super(props);
+const styles = {
+  propContainer: {
+    width: 200,
+    overflow: 'hidden',
+    margin: '20px auto 0',
+  },
+  propToggleHeader: {
+    margin: '20px auto 10px',
+  },
+};
+
+const competitions = [
+  { songTitle: "Song Title 1",
+    artistName:"Artist 1",
+    contestDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio."},
+  { songTitle: "Song Title 2",
+    artistName:"Artist 2",
+    contestDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio." },
+  { songTitle: "Song Title 3",
+    artistName:"Artist 3",
+    contestDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio." },
+  { songTitle: "Song Title 4",
+    artistName:"Artist 4",
+    contestDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio." },
+  { songTitle: "Song Title 5",
+    artistName:"Artist 5",
+    contestDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio." },
+  { songTitle: "Song Title 6",
+    artistName:"Artist 6",
+    contestDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio." },
+  { songTitle: "Song Title 7",
+    artistName:"Artist 7",
+    contestDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio." },
+  { songTitle: "Song Title 8",
+    artistName:"Artist 8",
+    contestDescription:"Lorem ipsum dolor sit amet, e lobortis odio." }
+
+];
+
+export default class TableExampleComplex extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fixedHeader: true,
+      fixedFooter: true,
+      stripedRows: true,
+      showRowHover: true,
+      selectable: false,
+      multiSelectable: false,
+      enableSelectAll: false,
+      deselectOnClickaway: true,
+      showCheckboxes: false,
+      height: 'initial',
+    };
   }
 
-  render(){
-	return(
-		<div> Competition list </div>
-		);
+  handleToggle = (event, toggled) => {
+    this.setState({
+      [event.target.name]: toggled,
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({height: event.target.value});
+  };
+
+  render() {
+    return (
+      <div>
+        <Table
+          height={this.state.height}
+          fixedHeader={this.state.fixedHeader}
+          fixedFooter={this.state.fixedFooter}
+          selectable={this.state.selectable}
+          multiSelectable={this.state.multiSelectable}
+        >
+          <TableHeader
+            displaySelectAll={this.state.showCheckboxes}
+            adjustForCheckbox={this.state.showCheckboxes}
+            enableSelectAll={this.state.enableSelectAll}
+          >
+            <TableRow>
+              <TableHeaderColumn className = "table-small">Song Title</TableHeaderColumn>
+              <TableHeaderColumn className = "table-small">Artist</TableHeaderColumn>
+              <TableHeaderColumn className = "table-small">Status</TableHeaderColumn>
+              <TableHeaderColumn className = "table-large">Description</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={this.state.showCheckboxes}
+            deselectOnClickaway={this.state.deselectOnClickaway}
+            showRowHover={this.state.showRowHover}
+            stripedRows={this.state.stripedRows}
+          >
+            {competitions.map( (row, index) => (
+              <TableRow key={index} selected={row.selected}>
+                <TableRowColumn className = "table-small">{row.songTitle}</TableRowColumn>
+                <TableRowColumn className = "table-small">{row.artistName}</TableRowColumn>
+                <TableRowColumn className = "table-small">Ongoing</TableRowColumn>
+                <TableRowColumn className = "table-large">{row.contestDescription}</TableRowColumn>
+                <TableRowColumn >{row.status}</TableRowColumn>
+              </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+
+      </div>
+    );
   }
 }
