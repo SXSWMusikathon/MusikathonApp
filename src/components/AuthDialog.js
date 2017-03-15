78bfc885489b84445c7d4f9751119fb6be6cb668
Nhat Dao <daoanhnhat1995@gmail.com>
2017-03-15 01:51:32 -0500

    added compare songs

--- src/components/AuthDialog.js
+++ src/components/AuthDialog.js
@@ -2,48 +2,75 @@ import React, {Component} from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import {GridList, GridTile} from 'material-ui/GridList';
+import Player from './Player';
 
 export default class AuthDialog extends Component {
   constructor(props){
 	super(props);
 	this.state = {
 	  open: false,
+	  isAudioPlaying: [false, false]
 	};
+
+	this.handleTogglePlay = this.handleTogglePlay.bind(this);
   }
 
   handleOpen = () => {
-    this.setState({open: true});
+	this.setState({open: true});
   };
 
+  handleTogglePlay = (playerId)=> {
+	let stopId = playerId == "0" ? "1" : "0";
+	this.refs[stopId]._pauseAudio();
+  }
+
   handleClose = () => {
-    this.setState({open: false});
+	this.setState({open: false});
   };
 
   render() {
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        primary={true}
-        onTouchTap={this.handleClose}
-      />,
-      <FlatButton
-        label="Submit"
-        primary={true}
-        keyboardFocused={true}
-        onTouchTap={this.handleClose}
-      />,
-    ];
+	const actions = [
+	  <FlatButton
+		label="Cancel"
+		primary={true}
+		onTouchTap={this.handleClose}
+	  />,
+	<FlatButton
+	  label="Submit"
+	  primary={true}
+	  keyboardFocused={true}
+	  onTouchTap={this.handleClose}
+	/>,
+	];
 
-    return (
-		<Dialog
-          title="Dialog With Actions"
-          actions={actions}
-          modal={false}
-          open={this.state.open}
-          onRequestClose={this.handleClose}
-        >
-          The actions in this window were passed in as an array of React objects.
-        </Dialog>
-    );
+
+	return (
+	  <Dialog
+		title="Dialog With Actions"
+		actions={actions}
+		modal={false}
+		open={this.state.open}
+		onRequestClose={this.handleClose}
+	  >
+		The actions in this window were passed in as an array of React objects.
+		<div className="player-flex-container">
+		  <Player 
+			isPlaying={this.state.isAudioPlaying} 
+			trackId="79973942"
+			playerId="0"
+			ref="0"
+			onTogglePlay={this.handleTogglePlay}
+		  />
+		  <Player 
+			isPlaying={this.state.isAudioPlaying}
+			trackId="47580057"
+			playerId="1"
+			ref="1"
+			onTogglePlay={this.handleTogglePlay}
+		  />
+		</div>
+	  </Dialog>
+	);
   }
 }
---
+++ src/components/Player.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { CLIENT_ID } from './../constants/Config';
+import { GridTile } from 'material-ui/GridList';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+import './Player.css';
+import testImage from './../images/SongArtTest.jpg';
+
+export default class Player extends Component{
+  constructor(props){
+	super(props);
+	this.state = {
+	  currentPlaying: false
+	};
+	this.onTogglePlay = this.onTogglePlay.bind(this);
+  }
+
+  formatStreamUrl(trackId) {
+	return `https://api.soundcloud.com/tracks/${trackId}/stream?client_id=${CLIENT_ID}`;
+  }
+
+  onTogglePlay(){
+	if (this.state.currentPlaying){
+	  this._pauseAudio();
+	} else {
+	  this.props.onTogglePlay(this.props.playerId);
+	  this._startAudio();
+	}
+  }
+
+  _pauseAudio(){
+	const trackId = this.props.trackId;
+	const audioElement = ReactDOM.findDOMNode(this.refs[trackId]);
+	console.log("Pasued");
+	audioElement.pause();
+	this.setState({currentPlaying: false});
+  }
+
+  _startAudio(){
+	const trackId = this.props.trackId;
+	const audioElement = ReactDOM.findDOMNode(this.refs[trackId]);
+	audioElement.play();
+	this.setState({currentPlaying: true});
+  }
+
+
+  render(){
+	const { 
+	  playerId, 
+	  trackId 
+	} = this.props;
+	const audioId = `audio_${trackId}`;
+
+	return (
+	  <div className="player">
+		<audio id={audioId} ref={trackId} src={this.formatStreamUrl(trackId)} />
+		<Card className="player-card">
+		  <CardMedia className="competition-card-image">
+			<img src={testImage}/>
+			<CardActions className="competition-card-actions">
+			  {/* <FlatButton onClick={this.onPlay} label="Play" /> */}
+			  {/* <a href="#" title="Play video" className="play"></a> */}
+			  {/* https://codepen.io/stevenfabre/pen/DvBei */}
+			  <button onClick={this.onTogglePlay}>
+			  </button>
+			</CardActions>
+		  </CardMedia>
+
+		</Card>
+	  </div>
+	)
+  }
+
+}
