31e52db4e74bbe19529a762d9f37a275d3f2ef25
HeyImRige <Robert.aron.tx@verizon.net>
2017-03-15 04:25:49 -0500

    Setup Competition Details Template

--- src/containers/CompetitionDetailPage.js
+++ src/containers/CompetitionDetailPage.js
@@ -1,28 +1,75 @@
 import React, { Component } from 'react';
-
+import './CompetitionDetailsPage.css';
+import myImage from '../images/SongArtTest.jpg'
+import CompetitionSteps from '../components/CompetitionSteps';
+import PrizeCard from '../components/PrizeCard'
 export default class CompetitionDetailPage extends Component { 
+
   constructor(props) {
 	  super(props);
-    const competitions = [
+    this.competitions = 
       {artImage: "../images/SongArtTest.jpg",
       songName:"My Song 1",
       artistName:"Artist 1",
-      Details:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-      SoundCloudLink:"https://soundcloud.com/ujico/lullaby",
-      Prizes:["Prize 1","Prize 2","Prize 3"],
-      StemsLink:"https://puu.sh/uJAa3/9a94e2da7e.png",
-      StartDate:"February 1st, 2017",
-      EndDate:"March 2nd, 2017",
-      WinnerAnnounced:"March 10th, 2017"
-      }];
+      details:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
+      soundCloudLink:"https://soundcloud.com/ujico/lullaby",
+      prizes:["Prize 1","Prize 2","Prize 3"],
+      stemsLink:"https://puu.sh/uJAa3/9a94e2da7e.png",
+      startDate:"February 1st, 2017",
+      endDate:"March 2nd, 2017",
+      winnerAnnounced:"March 10th, 2017"};
   }
 
+
+
+
+
+  renderTopDetails(){
+
+    return(
+      <div className="competition-details-top">
+        {/*http://stackoverflow.com/questions/13518833/making-all-photos-square-via-css*/}
+        <img src={myImage} className="competition-details-art"></img>
+        <div className="competition-details-details">
+          <h1 className="competition-details-title">Remix {this.competitions.songName}</h1>
+          <p>{this.competitions.details}</p>
+        </div>
+        <div className="competition-steps">
+          <CompetitionSteps/>
+        </div>
+      </div>
+    );
+  }
+
+  renderBotDetails(){
+    return(
+      <div className="details-bot-container">
+        <h1>PRIZES!</h1>
+        <div className="prize-card-div">
+          <PrizeCard place = "1"/>
+          <PrizeCard place = "2"/>
+          <PrizeCard place = "3"/>
+        </div>
+      </div>
+    )
+  }
+
+
   render() {
-	return (
-    <div className = "details-background">
-      <div> Detail { this.props.params.competitionId } </div>
-      <div> test</div>
-    </div>
-		);
+    return (
+      <div>
+        <div className = "details-background-top">
+          <div className = "details-container">
+            {this.renderTopDetails()}
+          </div>
+        </div>
+        <div className = "details-container">
+          <div>
+            {this.renderBotDetails()}
+          </div>
+        </div>
+        Extension: { this.props.params.competitionId }
+      </div>
+    );
   }
 }
